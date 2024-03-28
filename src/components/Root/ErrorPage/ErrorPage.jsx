39cbf1051ffb4a7f023cb8b8f91539780ea1b29d
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-03-28 23:04:06 +0600

    main.jsx updated

--- src/components/Root/ErrorPage/ErrorPage.jsx
+++ src/components/Root/ErrorPage/ErrorPage.jsx
@@ -3,8 +3,12 @@ import { Link } from "react-router-dom";
 const ErrorPage = () => {
     return (
         <div>
-           <h2>Oops!!</h2> 
-           <Link to="/">Go back to home</Link>
+           <div className="border border-indigo-200 rounded-3xl bg-sky-400">
+           <h2 className="text-center">Oops!!</h2> 
+           <h2 className="text-3xl text-center">404 Page</h2>
+           
+           <Link to="/" className="text-center">Go back to home</Link>
+           </div>
         </div>
     );
 };
